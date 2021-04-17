a9096e2482ebdbaacd161d65605655f28d3d9fa0
Suleyman Poyraz <zaryob.dev@gmail.com>
2021-04-17 12:51:43 +0300

    Map rotalarını düzenledim.

--- lib/routes/home/map_tab.dart
+++ lib/routes/home/map_tab.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:latlng/latlng.dart';
 import 'package:map/map.dart';
+import 'package:putty/network/map/get_location.dart';
 
 class MapTab extends StatefulWidget {
   @override
@@ -9,15 +10,19 @@ class MapTab extends StatefulWidget {
 
 class _MapTabState extends State<MapTab> {
   final controller = MapController(
-    location: LatLng(35.68, 51.41),
+    location: LatLng(39.92524128151174, 32.83692009925839),
   );
 
   void _gotoDefault() {
-    controller.center = LatLng(35.68, 51.41);
+    controller.center = LatLng(39.92524128151174, 32.83692009925839);
   }
 
   void _onDoubleTap() {
     controller.zoom += 0.5;
+  }
+
+  void _onNegativeDoubleTap() {
+    controller.zoom -= 0.5;
   }
 
   late Offset _dragStart;
@@ -46,38 +51,99 @@ class _MapTabState extends State<MapTab> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GestureDetector(
-        onDoubleTap: _onDoubleTap,
-        onScaleStart: _onScaleStart,
-        onScaleUpdate: _onScaleUpdate,
-        onScaleEnd: (details) {
-          print("Location: ${controller.center.latitude}, ${controller.center.longitude}");
-        },
-        child: Stack(
-          children: [
-            Map(
-              controller: controller,
-              builder: (context, x, y, z) {
-                final url =
-                    'https://www.google.com/maps/vt/pb=!1m4!1m3!1i$z!2i$x!3i$y!2m3!1e0!2sm!3i420120488!3m7!2sen!5e1105!12m4!1e68!2m2!1sset!2sRoadmap!4e0!5m1!1e0!23i4111425';
-
-                return Image.network(
-                  url,
-                  fit: BoxFit.cover,
-                );
+        body: Stack(
+          children: <Widget>[
+            GestureDetector(
+              onDoubleTap: _onDoubleTap,
+              onScaleStart: _onScaleStart,
+              onScaleUpdate: _onScaleUpdate,
+              onScaleEnd: (details) {
+                print(
+                    "Location: ${controller.center.latitude}, ${controller.center.longitude}");
               },
+              child: Stack(
+                children: [
+                  Map(
+                    controller: controller,
+                    builder: (context, x, y, z) {
+                      final url =
+                          'https://www.google.com/maps/vt/pb=!1m4!1m3!1i$z!2i$x!3i$y!2m3!1e0!2sm!3i420120488!3m7!2sen!5e1105!12m4!1e68!2m2!1sset!2sRoadmap!4e0!5m1!1e0!23i4111425';
+
+                      return Image.network(
+                        url,
+                        fit: BoxFit.cover,
+                      );
+                    },
+                  ),
+                  Center(
+                    child: Icon(Icons.close, color: Colors.red),
+                  ),
+                ],
+              ),
             ),
-            Center(
-              child: Icon(Icons.close, color: Colors.red),
+            Positioned(
+              top: 10,
+              right: 15,
+              left: 15,
+              child: Container(
+                color: Colors.white,
+                child: Row(
+                  children: <Widget>[
+                    IconButton(
+                      splashColor: Colors.grey,
+                      icon: Icon(Icons.menu),
+                      onPressed: () {},
+                    ),
+                    Expanded(
+                      child: TextField(
+                        cursorColor: Colors.black,
+                        keyboardType: TextInputType.text,
+                        textInputAction: TextInputAction.go,
+                        decoration: InputDecoration(
+                            border: InputBorder.none,
+                            contentPadding:
+                                EdgeInsets.symmetric(horizontal: 15),
+                            hintText: "Search..."),
+                      ),
+                    ),
+                    /*Padding(
+                      padding: const EdgeInsets.only(right: 8.0),
+                      child: CircleAvatar(
+                        backgroundColor: Colors.deepPurple,
+                        child: Text('RD'),
+                      ),
+                    ),*/
+                  ],
+                ),
+              ),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _gotoDefault,
-        tooltip: 'My Location',
-        child: Icon(Icons.my_location),
-      ),
-    );
+        floatingActionButton: Stack(children: <Widget>[
+          Align(
+            alignment: Alignment.bottomRight,
+            child: Column(
+              children: <Widget>[
+                FloatingActionButton(
+                  onPressed: _gotoDefault,
+                  tooltip: 'My Location',
+                  child: Icon(Icons.my_location),
+                ),
+                SizedBox(height: 8),
+                FloatingActionButton(
+                  onPressed: _onDoubleTap,
+                  tooltip: 'Bigger',
+                  child: Icon(Icons.add),
+                ),
+                SizedBox(height: 8),
+                FloatingActionButton(
+                  onPressed: _onNegativeDoubleTap,
+                  tooltip: 'Smaller',
+                  child: Icon(Icons.remove),
+                ),
+              ],
+            ),
+          )
+        ]));
   }
 }
